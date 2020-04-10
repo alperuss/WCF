73f7cfca855b2807b856315ccd05d3de1564a3b4
alper <alper.usss@gmail.com>
2020-04-10 13:52:18 +0300

    son

--- WCFServer/Data/IChat.cs
+++ WCFServer/Data/IChat.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.ServiceModel;
 using System.Text;
@@ -39,7 +40,26 @@ namespace Data
             
             });
             Console.WriteLine(nickname + ": " + message );
+            string path = @"C:\Users\Alper\Desktop\WCF\WCF\WCFServer\log.txt";
 
+            // This text is added only once to the file.
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                string createText = nickname + ": " + message + "--> " + DateTime.Now + Environment.NewLine;
+                File.WriteAllText(path, createText);
+            }
+            else
+            {
+                // This text is always added, making the file longer over time
+            // if it is not deleted.
+            string appendText = nickname + ": " + message + "--> " + DateTime.Now + Environment.NewLine;
+            File.AppendAllText(path, appendText);
+
+            //File.writealltext
+            //file.appendtext
+            }
+            
         }
 
         public List<MessageDto> GetAllMessages()
